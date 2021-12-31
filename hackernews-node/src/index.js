1cae093028b7426f291fe51e68742ccdbb838a16
spanda <santanu.panda@gcp.lowes.com>
2021-12-31 13:38:45 +0530

    Started Odyssey Apollo Lift Off 1

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
@@ -8,12 +9,18 @@ const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
+const pubsub = new PubSub();
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const prisma = new PrismaClient();
@@ -25,6 +32,7 @@ const server = new ApolloServer({
     return {
       ...req,
       prisma,
+      pubsub,
       userId: req && req.headers.authorization ? getUserId(req) : null,
     };
   },
